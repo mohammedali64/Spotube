f27e2cfe2f5a53e6eef269afeb866fa75488b416
mohammedali64 <mohammedali88870@gmail.com>
2024-12-18 22:52:59 +0530

    Update useGetFeaturedPlaylist.js
    get featured playlist api is not working

--- src/hooks/useGetFeaturedPlaylist.js
+++ src/hooks/useGetFeaturedPlaylist.js
@@ -2,17 +2,26 @@ import React, { useEffect, useState } from 'react'
 
 const useGetFeaturedPlaylist = () => {
     const [playlists, setPlaylists] = useState([]);
-    const token = localStorage.getItem('spotify_access_token'); // Assuming the token is stored in localStorage
+    const token = localStorage.getItem("spotify_access_token"); 
   
     const fetchFeaturedPlaylists = async () => {
-      const response = await fetch(`https://api.spotify.com/v1/browse/featured-playlists`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      
-      const data = await response.json();
-      setPlaylists(data.playlists.items);
+      try {
+        const response = await fetch(`https://api.spotify.com/v1/browse/featured-playlists`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+    
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    
+        const data = await response.json();
+        console.log(data);
+        setPlaylists(data.playlists.items);
+      } catch (error) {
+        console.error('Error fetching featured playlists:', error);
+      }
     };
 
   
